00d394e26acf20d5029afea41f8806f54f834d92
Alexis Montoya <afmontoya2@utpl.edu.ec>
2020-07-10 00:39:54 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import{ MiComponente } from './componets/mi-componente/mi-componente.component';
+import { from } from 'rxjs';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiComponente
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
